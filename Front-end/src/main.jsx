3bb5aa55ef51ee651a4493b0a18bcff808b25474
sameerofficial545-ctrl <sameer@example.com>
2026-09-04 15:25:23 +0530

    Refresh Eventify with cohesive aurora theme

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import ParticleField from './components/ParticleField.jsx'
 import './index.css'
+import './theme.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
